301512b540a33ae103c6b2e957f4eba630a82228
root <root@Thousand-Sunny>
2023-08-19 15:01:13 -0400

    Successfully modularized the side bar.

--- client/components/NavSidePanel.jsx
+++ client/components/NavSidePanel.jsx
@@ -1,29 +1,26 @@
 // handle redirect fires
 
-
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-
-
-
-const NavSidePanel=()=>{
-
-let navigate = useNavigate();
+const NavSidePanel = () => {
+  let navigate = useNavigate();
   const toCreate = () => {
-      let path = '/create';
-      navigate(path);
-  }
-
-  <div className='navSidePanel'>
-
-          <button className='navButton'>My Plants</button>
-          <hr/>
-          <button className='navButton' onClick={toCreate}>New Plants</button>
-          <hr/>
-          <button className='navButton'>Discover</button>
-          
-        </div>
-}
+    let path = '/create';
+    navigate(path);
+  };
+  
+  return (
+    <div className="navSidePanel">
+      <button className="navButton">My Plants</button>
+      <hr />
+      <button className="navButton" onClick={toCreate}>
+        New Plants
+      </button>
+      <hr />
+      <button className="navButton">Discover</button>
+    </div>
+  );
+};
 
 export default NavSidePanel;
--- client/containers/MainPage.jsx
+++ client/containers/MainPage.jsx
@@ -7,7 +7,6 @@
  * PopUp
  */
 
-
 // ====== COMPONENTS ======
 /**
  * Plant Display
@@ -18,64 +17,45 @@
 
 import React from 'react';
 import { useSelector } from 'react-redux';
-import PlantDisplay from '../components/PlantDisplay.jsx'
-import Plant from '../components/PlantCard.jsx'
-import {useNavigate} from 'react-router-dom';
-
+import PlantDisplay from '../components/PlantDisplay.jsx';
+import Plant from '../components/PlantCard.jsx';
+import { useNavigate } from 'react-router-dom';
+import NavSidePanel from '../components/NavSidePanel.jsx';
 const MainPage = () => {
-
-  const totalCards = useSelector(store => store.plants.totalCards);
-  const plantList = useSelector(store => store.plants.plantList);
-	const greenScore = 1000;
-
-  let navigate = useNavigate();
-  const toCreate = () => {
-      let path = '/create';
-      navigate(path);
-  }
-
+  
+  const totalCards = useSelector((store) => store.plants.totalCards);
+  const plantList = useSelector((store) => store.plants.plantList);
+  const greenScore = 1000;
 
   return (
     <div>
-      <div className='nav-bar'>
+      <div className="nav-bar">
         <h1>LeafLaughLove</h1>
       </div>
-      <div className='homePageContainer'>
+      <div className="homePageContainer">
 
-        <div className='navSidePanel'>
+        <NavSidePanel/>
 
-          <button className='navButton'>My Plants</button>
-          <hr/>
-          <button className='navButton' onClick={toCreate}>New Plants</button>
-          <hr/>
-          <button className='navButton'>Discover</button>
-          
-        </div>
-        
-        <div className='plantContainer'>
-
-          <div className='totalPlantDataBox'>
-            <div class='totalPlantData'>
-							{/* Total Plants: {totalCards} */}
-							Total Plants: 6421654
-						</div>
-						<div class='totalPlantData'>
-							Green Score: {greenScore}
-						</div>
-						<div class='totalPlantData'>
-							Filter Plants
-						</div>
+        <div className="plantContainer">
+          <div className="totalPlantDataBox">
+            <div class="totalPlantData">
+              {/* Total Plants: {totalCards} */}
+              Total Plants: 6421654
+            </div>
+            <div class="totalPlantData">Green Score: {greenScore}</div>
+            <div class="totalPlantData">Filter Plants</div>
           </div>
-          
-          <div className='plantDisplay'>
+
+          <div className="plantDisplay">
             {/* <PlantDisplay
               // include plantList
             /> */}
-						<PlantDisplay />
+            <PlantDisplay />
           </div>
         </div>
       </div>
     </div>
-  )}
+  );
+};
 
 export default MainPage;
